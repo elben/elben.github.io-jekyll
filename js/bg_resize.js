2b1fe53fa2cac3a2290904a9d6fb24cde1380f9f
Elben Shira <elbenshira@gmail.com>
2013-10-22 20:58:54 -0500

    Move JS.

---
+++ js/bg_resize.js
@@ -0,0 +1,29 @@
+
+//var bgImgWidthHeight = 1.605; // ratio for background.jpg
+var bgImgWidthHeight = 1.689; // ratio for background2.jpg
+
+resizeBg = function() {
+  var wh = $(window).height();
+  var ww = $(window).width();
+  if(ww/wh > bgImgWidthHeight) {
+    // Widow is more "widthy" than the image
+    $("img#background").removeAttr("height");
+    $("img#background").attr("width", ww);
+    // TODO: Also set the cut offs so that the scroll bars don't appear if not
+    // needed
+  } else {
+    // Widow is more "heighty" than the image
+    $("img#background").removeAttr("width");
+    $("img#background").attr("height", wh);
+  }
+}
+
+
+$(window).resize(function() {
+    resizeBg();
+});
+
+$().ready(function() {
+  resizeBg();
+});
+
